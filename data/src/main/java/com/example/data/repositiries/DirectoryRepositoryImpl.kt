f142b5f9ca937de06f7a9d10bd0baa3c17b173fa
Nik <kolya-buldog@inbox.ru>
2024-08-20 00:26:59 +0300

    c

--- data/src/main/java/com/example/data/repositiries/DirectoryRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repositiries/DirectoryRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.example.data.repositiries
 import com.example.data.model.database.DirectoryDao
 import com.example.domain.model.DirectoryDomain
 import com.example.domain.repositories.DirectoryRepository
+import com.example.utils.toData
 import com.example.utils.toDomain
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -11,7 +12,7 @@ import kotlinx.coroutines.withContext
 class DirectoryRepositoryImpl(private val directoryDao: DirectoryDao) : DirectoryRepository {
     override suspend fun insertContact(directoryDomain: DirectoryDomain) {
         return withContext(Dispatchers.IO) {
-            directoryDao.insert(directoryDomain.toDomain())
+            directoryDao.insert(directoryDomain.toData())
         }
     }
 
--- data/src/main/java/com/example/utils/MapperDataExt.kt
+++ data/src/main/java/com/example/utils/MapperDataExt.kt
@@ -3,7 +3,7 @@ package com.example.utils
 import com.example.data.model.DirectoryData
 import com.example.domain.model.DirectoryDomain
 
-fun DirectoryDomain.toDomain() = DirectoryData(
+fun DirectoryDomain.toData() = DirectoryData(
     id = id, name = name, phoneNumber = phoneNumber
 )
 
