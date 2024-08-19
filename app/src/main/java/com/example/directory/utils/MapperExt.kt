c2a7a40c8d0d4c183cdff0700cb341d58baa3540
Nik <kolya-buldog@inbox.ru>
2024-08-19 23:43:25 +0300

    c

--- app/src/main/java/com/example/directory/utils/MapperExt.kt
+++ app/src/main/java/com/example/directory/utils/MapperExt.kt
@@ -1,9 +1,10 @@
 package com.example.directory.utils
 
+import com.example.data.model.DirectoryData
 import com.example.directory.ui.model.Directory
 import com.example.domain.model.DirectoryDomain
 
-fun DirectoryDomain.toDomain() = Directory(
+fun DirectoryData.toDomain() = DirectoryDomain(
     id = id, name = name, phoneNumber = phoneNumber
 )
 
